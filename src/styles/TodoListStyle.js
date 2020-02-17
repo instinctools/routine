8497d2df1ec40c7d88a80b063e07d10388925e05
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-02-17 12:45:34 +0300

    simple todo list

---
+++ src/styles/TodoListStyle.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const style = StyleSheet.create({
+    item: {
+        flexDirection: `row`,
+        padding: 16
+    },
+    itemTitle: {
+        flex: 1
+    }
+});
--- src/ui/TodoList.js
+++ src/ui/TodoList.js
@@ -1,13 +1,32 @@
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import React from 'react';
+import { style } from '../styles/TodoListStyle';
+
+
+const createTodos = (count) => {
+    let todos = [];
+    for (let i = 0; i < count; i++) {
+        todos.push({
+            title: `TODO ${i}`,
+            timestamp: 333422323
+        })
+    }
+    return todos
+};
 
 export class TodoList extends React.Component {
 
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>TodoList</Text>
-            </View>
+            <FlatList style = {style.container}
+                data={createTodos(200)}
+                renderItem={({item}) =>
+                    <View style = {style.item}>
+                        <Text style = {style.itemTitle}>{item.title}</Text>
+                        <Text>{item.timestamp}</Text>
+                    </View>
+                }
+            />
         );
     }
 }
