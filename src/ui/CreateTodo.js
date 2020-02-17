a466f53af353d05beaa223c582dc16c62d8046ed
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-02-17 11:03:31 +0300

    Simple navigation between TodoList and CreateTodo screens

---
+++ src/ui/CreateTodo.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export class CreateTodo extends React.Component {
+
+    render() {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>CreateTodo</Text>
+            </View>
+        );
+    }
+}
---
+++ src/ui/TodoList.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export class TodoList extends React.Component {
+
+    render() {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>TodoList</Text>
+            </View>
+        );
+    }
+}
