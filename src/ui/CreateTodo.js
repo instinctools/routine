de998e3f721739f559f142ec60f96cf87b6cc6b7
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-02-17 17:57:50 +0300

    simple create todo screen

--- src/ui/CreateTodo.js
+++ src/ui/CreateTodo.js
@@ -1,12 +1,35 @@
-import {Text, View} from 'react-native';
+import {Button, Picker, TextInput, View} from 'react-native';
 import React from 'react';
 
+const UNIT = ["DAY", "WEEK", "MONTH"];
+
 export class CreateTodo extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "",
+            amount: 1,
+            unit: UNIT[0]
+        }
+    }
 
     render() {
         return (
-            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>CreateTodo</Text>
+            <View>
+                <TextInput
+                    placeholder="Enter amount"
+                    onChangeText={text => this.setState({title: {text}})}
+                />
+                <Picker
+                    selectedValue={this.state.unit}
+                    onValueChange={(itemValue, _) => this.setState({unit: itemValue})}>
+                    <Picker.Item label={UNIT[0]} value={UNIT[0]}/>
+                    <Picker.Item label={UNIT[1]} value={UNIT[1]}/>
+                    <Picker.Item label={UNIT[2]} value={UNIT[2]}/>
+                </Picker>
+                <Button title={`SAVE CHANGES`} onPress={() =>
+                    this.props.navigation.pop()
+                }/>
             </View>
         );
     }
