dda13e3c9a2e65142c17ea2b78de1eac00073c36
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-02-17 17:20:40 +0300

    navigate to create task

--- src/ui/TodoList.js
+++ src/ui/TodoList.js
@@ -1,7 +1,7 @@
-import {FlatList, Text, View} from 'react-native';
+import {FlatList, Text, View, Button} from 'react-native';
 import React from 'react';
 import { style } from '../styles/TodoListStyle';
-
+import {CreateTodo} from '../ui/CreateTodo';
 
 const createTodos = (count) => {
     let todos = [];
@@ -18,15 +18,22 @@ export class TodoList extends React.Component {
 
     render() {
         return (
-            <FlatList style = {style.container}
-                data={createTodos(200)}
-                renderItem={({item}) =>
-                    <View style = {style.item}>
-                        <Text style = {style.itemTitle}>{item.title}</Text>
-                        <Text>{item.timestamp}</Text>
-                    </View>
-                }
-            />
+            <View style = {{position: "relative"}}>
+                <Button title="Add task"
+                        onPress={() =>
+                            this.props.navigation.navigate(CreateTodo.name)
+                        }/>
+                <FlatList style = {style.container}
+                          data={createTodos(200)}
+                          renderItem={({item}) =>
+                              <View style = {style.item}>
+                                  <Text style = {style.itemTitle}>{item.title}</Text>
+                                  <Text>{item.timestamp}</Text>
+                              </View>
+                          }
+                />
+            </View>
+
         );
     }
 }
