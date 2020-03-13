c0bfbe31298b3b645b8a6ef71a038b23108caf01
Sergey Shostko <s.shostko@gmail.com>
2020-03-13 16:31:22 +0300

    add persisting todos

--- src/store.js
+++ src/store.js
@@ -1,6 +1,14 @@
 import {combineReducers, createStore} from 'redux';
 import {reducer as todos} from './reducer/todos';
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+    key: 'todos',
+    storage: AsyncStorage,
+};
 
 export const store = createStore(combineReducers({
-    todos
+    todos: persistReducer(persistConfig, todos)
 }));
+export const persistor = persistStore(store);
