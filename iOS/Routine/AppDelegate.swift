d3cf9afc4d011ac9625bac63440e658be0ffc24d
Vadzim Karonchyk <vadzim.karonchyk@instinctools.com>
2020-03-20 17:48:28 +0300

    Created project for iOS

---
+++ iOS/Routine/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Routine
+//
+//  Created by Vadzim Karonchyk on 3/20/20.
+//  Copyright © 2020 Instinctools. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ iOS/Routine/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Routine
+//
+//  Created by Vadzim Karonchyk on 3/20/20.
+//  Copyright © 2020 Instinctools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
