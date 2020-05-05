4dcf547250f5ce6d8ff705afd5eeb2b0db4b3e7c
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-05-05 16:58:54 +0300

    basic app color theming

--- Android/app/src/main/java/com/instinctools/routine_android/MainActivity.kt
+++ Android/app/src/main/java/com/instinctools/routine_android/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.instinctools.routine_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.instinctools.routine_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.toolbar.setOnMenuItemClickListener {
+            startActivity(Intent(this, DetailsActivity::class.java))
+            true
+        }
     }
 }
