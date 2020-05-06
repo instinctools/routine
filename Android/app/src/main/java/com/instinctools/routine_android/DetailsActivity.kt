c911ce38333abf47280d7686e57234ea9d4824ea
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-05-06 16:05:07 +0300

    details toolbar

--- Android/app/src/main/java/com/instinctools/routine_android/DetailsActivity.kt
+++ Android/app/src/main/java/com/instinctools/routine_android/DetailsActivity.kt
@@ -2,11 +2,26 @@ package com.instinctools.routine_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.MenuItemCompat
+import com.instinctools.routine_android.databinding.ActivityDetailsBinding
+import com.instinctools.routine_android.databinding.ActivityMainBinding
 
 class DetailsActivity : AppCompatActivity() {
 
+    private val binding: ActivityDetailsBinding by viewBinding(ActivityDetailsBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        setContentView(binding.root)
+
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        binding.toolbar.menu.findItem(R.id.done)
+            .actionView.setOnClickListener {
+
+            }
     }
 }
