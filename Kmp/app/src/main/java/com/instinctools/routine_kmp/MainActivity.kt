b8227ace4daada8962914e50bb41dce5c7200358
Pavel Horbach <pavel.horbach@instinctools.com>
2020-05-14 11:38:05 +0300

    make android runnable

---
+++ Kmp/app/src/main/java/com/instinctools/routine_kmp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.instinctools.routine_kmp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("asd", "hello")
+    }
+}
