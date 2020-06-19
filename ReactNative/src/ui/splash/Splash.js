0c2c3cd5a6e5b8b569b60f88d71b32cf75a5c2b2
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-06-19 17:54:00 +0300

    add error state to Splash screen

--- ReactNative/src/ui/splash/Splash.js
+++ ReactNative/src/ui/splash/Splash.js
@@ -1,7 +1,9 @@
 import React from "react";
-import {ActivityIndicator, Image, Text, View} from "react-native";
+import {ActivityIndicator, Image, Text, View, Button} from "react-native";
 import {connect} from "react-redux";
 import SplashAction from "../../action/SplashAction";
+import {TouchableRipple} from "react-native-paper";
+import analytics from "@react-native-firebase/analytics";
 
 class DetailsScreen extends React.Component {
     static navigationOptions = {
@@ -9,27 +11,51 @@ class DetailsScreen extends React.Component {
     };
 
     render() {
-        let splash = this.props.splash
+        let {isProgress, isSuccess, isError} = this.props.splash
+        if (isSuccess){
+            props.navigation.navigate("Items")
+        }
+        let text = isProgress ? "Setting up account" : "An error occurred!"
+
         return <View style={{
             flex: 1,
-            alignItems: 'stretch'
         }}>
             <View style={{flex: 1, justifyContent: 'center'}}>
                 <Image
-                    style={{width: 150, height: 150, alignSelf: `center`}}
-                    source={require('./ic_launcher.png')}
+                    style={{width: 196, height: 196, alignSelf: `center`}}
+                    //TODO Check if it possible to replace by svg
+                    source={require('./splash.png')}
                 />
 
             </View>
 
-            <View style={{flex: 1}}>
+            <View style={{flex: 1, alignItems: 'center'}}>
                 <Text style={{
                     color: `#835D51`,
                     textAlign: "center",
                     fontSize: 18,
                     fontWeight: 'bold'
-                }}>Setting up account</Text>
-                {getProgress()}
+                }}>{text}</Text>
+                {
+                    isProgress ?
+                        getProgress() :
+                        <TouchableRipple
+                            style ={{
+                                justifyContent: `center`,
+                                alignItems: `center`,
+                                marginTop: 32,
+                                borderRadius: 8,
+                                backgroundColor: `#835D51`,
+                                width: 156,
+                                height: 50}}
+                            onPress={() => {
+
+                            }}
+                            borderless={true}
+                        >
+                            <Text style={{fontSize: 16, color: `#ffffff`}}>Retry</Text>
+                        </TouchableRipple>
+                }
             </View>
         </View>
     }
@@ -38,7 +64,7 @@ class DetailsScreen extends React.Component {
 
 const getProgress = () => {
     return (Platform.OS === 'ios') ?
-        <ActivityIndicator size="large"/> :
+        <ActivityIndicator style={{marginTop: 32}} size="large"/> :
         <ActivityIndicator style={{marginTop: 32}} color="#835D51" size={48}/>
 }
 
