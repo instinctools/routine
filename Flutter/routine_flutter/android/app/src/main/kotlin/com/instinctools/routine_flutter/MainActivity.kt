66fd0c76a0fd27d9cb80ab44ad6f28ab3fb377db
Yahor Shymanchyk <yahor.shymanchyk@instinctools.com>
2020-09-08 16:42:11 +0300

    (Flutter) add logic with addReminder/cancelReminder called with methodChannel

--- Flutter/routine_flutter/android/app/src/main/kotlin/com/instinctools/routine_flutter/MainActivity.kt
+++ Flutter/routine_flutter/android/app/src/main/kotlin/com/instinctools/routine_flutter/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.instinctools.routine_flutter
 
-import androidx.annotation.NonNull;
+import android.util.Log
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "routine.flutter/reminder"
+    private val ADD_REMINDER = "addReminder"
+    private val CANCEL_REMINDER = "cancelReminder"
+    private val KEY_TODO_ID = "keyTodoId"
+    private val KEY_TODO_MESSAGE = "keyTodoMessage"
+    private val KEY_TODO_TARGET_DATE = "keyTodoTargetDate"
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            // Note: this method is invoked on the main thread.
+            call, result ->
+            when (call.method) {
+                ADD_REMINDER -> {
+                    Log.d("mainActivity", "ADD_REMINDER call.arguments = ${call.arguments}");
+                    val id = call.argument<String>(KEY_TODO_ID)
+                    val message = call.argument<String>(KEY_TODO_MESSAGE)
+                    val targetDate = call.argument<Long>(KEY_TODO_TARGET_DATE)
+                    //addReminder(id, message, targetDate)
+                }
+                CANCEL_REMINDER -> {
+                    Log.d("mainActivity", "CANCEL_REMINDER call.arguments = ${call.arguments}");
+                    val id = call.argument<String>(KEY_TODO_ID)
+                    //cancelReminder()
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
     }
 }
--- Flutter/routine_flutter/lib/repository/mainRepository.dart
+++ Flutter/routine_flutter/lib/repository/mainRepository.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:routine_flutter/data/todo.dart';
 
 class MainRepository {
+  static const methodAddReminder = "addReminder";
+  static const methodCancelReminder = "cancelReminder";
+  static const keyTodoId = "keyTodoId";
+  static const keyTodoMessage = "keyTodoMessage";
+  static const keyTodoTargetDate = "keyTodoTargetDate";
+  static const methodChannel = const MethodChannel('routine.flutter/reminder');
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   Firestore _fireStore = Firestore.instance;
   CollectionReference _collectionReference;
@@ -15,16 +22,19 @@ class MainRepository {
   void addTodo(Todo todo) {
     print("add Todo");
     _collectionReference.add(todo.toMap());
+    _addReminderToNative(todo);
   }
 
   void updateTodo(Todo todo) {
     print("update Todo");
     todo.reference.updateData(todo.toMap());
+    _addReminderToNative(todo);
   }
 
   void deleteTodo(Todo todo) {
     print("delete Todo");
     todo.reference.delete();
+    _cancelReminderToNative(todo);
   }
 
   Future<void> signInAnonymously() async {
@@ -37,5 +47,35 @@ class MainRepository {
         }
       },
     );
+  }
+
+  Future<void> _addReminderToNative(Todo todo) async {
+    try {
+      await methodChannel.invokeMethod(
+        methodAddReminder,
+        {
+          keyTodoId: todo.id,
+          keyTodoMessage: todo.title,
+          keyTodoTargetDate: todo.targetDate,
+        },
+      );
+      print("_addReminderToNative done. todo = $todo");
+    } on PlatformException catch (e) {
+      print("_addReminderToNative PlatformException = $e");
+    }
+  }
+
+  Future<void> _cancelReminderToNative(Todo todo) async {
+    try {
+      await methodChannel.invokeMethod(
+        methodCancelReminder,
+        {
+          keyTodoId: todo.id,
+        },
+      );
+      print("_cancelReminderToNative done. todo = $todo");
+    } on PlatformException catch (e) {
+      print("_cancelReminderToNative PlatformException = $e");
+    }
   }
 }
