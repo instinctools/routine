ee7f3d8880b59cb4bcba4f59bdaf9ece425dceac
Vadzim Korzun <vadzim.korzun@instinctools.com>
2020-10-16 11:13:14 +0300

    (Andoid) removed pausing dispatchers

--- Merged/android/app/src/test/java/com/routine/vm/AndroidAppViewModelTest.kt
+++ Merged/android/app/src/test/java/com/routine/vm/AndroidAppViewModelTest.kt
@@ -27,7 +27,6 @@ class AndroidAppViewModelTest {
 
     @Before
     fun init() {
-        commonRule.pauseDispatcher()
         androidAppViewModel = AndroidAppViewModel(commonRule.todosRepository)
     }
 
@@ -38,7 +37,6 @@ class AndroidAppViewModelTest {
         commonRule.todosRepository.todosStoreData = todoEntities
         androidAppViewModel.todosData
             .test {
-                commonRule.resumeDispatcher()
                 assertEquals(expectItem(), StoreResponse.Data(todoItems, ResponseOrigin.Fetcher))
                 cancelAndIgnoreRemainingEvents()
             }
