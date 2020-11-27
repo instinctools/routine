0a3f77eeae3f89d824f3f85e014d6012305bc739
Pavel Horbach <pavel.horbach@instinctools.com>
2020-11-27 14:35:53 +0300

    fix database R/W for dates

--- Kmp/shared/src/commonMain/kotlin/com/instinctools/routine_kmp/util/Dates.kt
+++ Kmp/shared/src/commonMain/kotlin/com/instinctools/routine_kmp/util/Dates.kt
@@ -1,8 +1,7 @@
 package com.instinctools.routine_kmp.util
 
-import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.todayAt
+import kotlinx.datetime.*
 
 fun currentDate(): LocalDate = Clock.System.todayAt(TimeZone.currentSystemDefault())
+fun dateFromEpoch(timestamp: Long, timeZone: TimeZone = TimeZone.currentSystemDefault()) = Instant.fromEpochSeconds(timestamp).toLocalDateTime(timeZone).date
+val LocalDate.timestampSystemTimeZone get() = atStartOfDayIn(TimeZone.currentSystemDefault()).epochSeconds
