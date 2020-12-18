1081d32def4a8fa237ce55a5fcf6852e332e3f0d
Pavel Horbach <pavel.horbach@instinctools.com>
2020-12-18 17:49:36 +0300

    create KMP runner

---
+++ Merged/android/app/src/main/java/com/routine/ui/KmpAppFragment.kt
@@ -0,0 +1,46 @@
+package com.routine.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.instinctools.routine_kmp.di.AppComponent
+import com.instinctools.routine_kmp.di.ComponentsProvider
+import com.instinctools.routine_kmp.di.DaggerAppComponent
+import com.instinctools.routine_kmp.ui.BaseFragment
+import com.instinctools.routine_kmp.ui.RootNavigator
+import com.instinctools.routine_kmp.ui.list.TodoListFragment
+import com.routine.R
+import dev.chrisbanes.insetter.Insetter
+import dev.chrisbanes.insetter.Side
+
+class KmpAppFragment : BaseFragment(R.layout.fragment_kmp_app), RootNavigator, ComponentsProvider {
+
+    override lateinit var appComponent: AppComponent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        appComponent = DaggerAppComponent.factory().create(requireContext().applicationContext)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Insetter.builder()
+            .applySystemWindowInsetsToMargin(Side.TOP)
+            .applyToView(view)
+
+        if (savedInstanceState == null) {
+            goto(TodoListFragment.newInstance(), addToBackStack = false)
+        }
+    }
+
+    override fun goBack() {
+        childFragmentManager.popBackStackImmediate()
+    }
+
+    override fun goto(fragment: Fragment, addToBackStack: Boolean) {
+        childFragmentManager.commit {
+            replace(R.id.kmp_fragment_container, fragment)
+            if (addToBackStack) addToBackStack(null)
+        }
+    }
+}
