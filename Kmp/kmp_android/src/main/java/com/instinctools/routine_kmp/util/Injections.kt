2f07e8c6e0cb330b77af30aefa3b692a6d0c644c
Pavel Horbach <pavel.horbach@instinctools.com>
2020-12-18 17:46:09 +0300

    expect navigator and injector from more places

--- Kmp/kmp_android/src/main/java/com/instinctools/routine_kmp/util/Injections.kt
+++ Kmp/kmp_android/src/main/java/com/instinctools/routine_kmp/util/Injections.kt
@@ -9,13 +9,6 @@ import com.instinctools.routine_kmp.di.ComponentsProvider
 import com.instinctools.routine_kmp.ui.PresenterContainer
 import com.instinctools.routine_kmp.ui.RootNavigator
 
-val Fragment.injector: AppComponent
-    get() {
-        val application = requireContext().applicationContext
-        require(application is ComponentsProvider) { "Application should extend ComponentsProvider" }
-        return application.appComponent
-    }
-
 inline fun <reified Presenter> Fragment.presenterContainer(
     crossinline provider: () -> Presenter
 ) = viewModels<PresenterContainer<Presenter>> {
@@ -24,9 +17,31 @@ inline fun <reified Presenter> Fragment.presenterContainer(
     }
 }
 
+val Fragment.injector: AppComponent
+    get() {
+        val provider = findFirstResponder<ComponentsProvider>()
+        checkNotNull(provider) { "One of parents should extend ComponentsProvider" }
+        return provider.appComponent
+    }
+
 val Fragment.rootNavigator: RootNavigator
     get() {
-        val activity = requireActivity()
-        require(activity is RootNavigator) { "Parent activity should have RootNavigator" }
-        return activity
+        val navigator = findFirstResponder<RootNavigator>()
+        return checkNotNull(navigator) { "One of parents should extend RootNavigator" }
     }
+
+inline fun <reified T> Fragment.findFirstResponder(): T? {
+    val application = requireContext().applicationContext
+    if (application is T) return application
+
+    val activity = requireActivity()
+    if (activity is T) return activity
+
+    var parent = parentFragment
+    while (parent != null) {
+        if (parent is T) return parent
+        parent = parent.parentFragment
+    }
+
+    return null
+}
